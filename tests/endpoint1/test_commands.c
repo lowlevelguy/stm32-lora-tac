482d935f76fc5c92571a2426b26283900340b4e3
lowlevelguy <virtual.code@protonmail.com>
2026-07-28 15:23:44 +0100

    `endpoint1::test_commands.c`: Same as the last commit.

--- tests/endpoint1/test_commands.c
+++ tests/endpoint1/test_commands.c
@@ -31,7 +31,7 @@ static void test_actuators_table_initialized(void) {
 static void test_led_red_command_turn_off_calls_gpio_reset_and_returns_ok(void) {
 	uint8_t params[3] = { LED_RED_COMMAND_TURN_OFF, 0, 0 };
 
-	HAL_GPIO_WritePin_Expect(LED3_GPIO_PORT, LED3_PIN, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin_Expect(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
 	uint8_t status = actuators[ACTUATOR_ID_LED_RED].command(params);
 	TEST_ASSERT_EQUAL_UINT8(COMMAND_STATUS_OK, status);
 }
@@ -39,7 +39,7 @@ static void test_led_red_command_turn_off_calls_gpio_reset_and_returns_ok(void) 
 static void test_led_red_command_turn_on_calls_gpio_set_and_returns_ok(void) {
 	uint8_t params[3] = { LED_RED_COMMAND_TURN_ON, 0, 0 };
 
-	HAL_GPIO_WritePin_Expect(LED3_GPIO_PORT, LED3_PIN, GPIO_PIN_SET);
+	HAL_GPIO_WritePin_Expect(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
 	uint8_t status = actuators[ACTUATOR_ID_LED_RED].command(params);
 	TEST_ASSERT_EQUAL_UINT8(COMMAND_STATUS_OK, status);
 }
@@ -56,7 +56,7 @@ static void test_led_red_command_unknown_opcode_does_not_touch_gpio_and_returns_
 static void test_led_red_command_ignores_trailing_param_bytes(void) {
 	uint8_t params[3] = { COMMAND_OPCODE_TURN_OFF, 0xAA, 0xBB };
 
-	HAL_GPIO_WritePin_Expect(LED3_GPIO_PORT, LED3_PIN, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin_Expect(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_RESET);
 	uint8_t status = actuators[ACTUATOR_ID_LED_RED].command(params);
 	TEST_ASSERT_EQUAL_UINT8(COMMAND_STATUS_OK, status);
 }
